a73dfed5deb2fad42942145961c414b3466ad411
dokyan1989 <dokyan1989@gmail.com>
2020-01-07 06:50:25 +0700

    Returning JSON

--- index.js
+++ index.js
@@ -30,14 +30,14 @@ var server = http.createServer(function (req, res) {
   // Get the payload, if any
   var decoder = new StringDecoder('utf-8');
   var buffer = '';
-  req.on('data', function(data) {
+  req.on('data', function (data) {
     buffer += decoder.write(data);
   });
-  req.on('end', function() {
+  req.on('end', function () {
     buffer += decoder.end();
 
     // Choose the handler this request should go to. If one is not found, use the notFound handler.
-    var chosenHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
+    var chosenHandler = typeof (router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
 
     // Construct the data object to send to the handler
     var data = {
@@ -49,24 +49,25 @@ var server = http.createServer(function (req, res) {
     };
 
     // Route the request to the handler specified in the router
-    chosenHandler(data, function(statusCode, payload) {
+    chosenHandler(data, function (statusCode, payload) {
       // Use the status code called back by the handler, or default to 200
-      statusCode = typeof(statusCode) == 'number' ? statusCode:200;
+      statusCode = typeof (statusCode) == 'number' ? statusCode : 200;
 
       // Use the payload called back by the handler, or default to an empty object
-      payload = typeof(payload) == 'object'?payload: {};
+      payload = typeof (payload) == 'object' ? payload : {};
 
       // Convert the payload to a string
       var payloadString = JSON.stringify(payload);
 
       // Return the response
+      res.setHeader('Content-Type', 'application/json');
       res.writeHead(statusCode);
       res.end(payloadString);
 
       // Log the request path
       console.log('Returning this response: ', statusCode, payloadString);
-    });    
-  });  
+    });
+  });
 });
 
 // Start the server, and have it listen on port 3000
@@ -78,13 +79,13 @@ server.listen(3000, function () {
 var handlers = {};
 
 // Sample handler
-handlers.sample = function(data, callback) {
+handlers.sample = function (data, callback) {
   // Callback a http status code, and a payload object
-  callback(406, {'name': 'sample handler'});
+  callback(406, { 'name': 'sample handler' });
 };
 
 // Not found handler
-handlers.notFound = function(data, callback) {
+handlers.notFound = function (data, callback) {
   callback(404);
 };
 
